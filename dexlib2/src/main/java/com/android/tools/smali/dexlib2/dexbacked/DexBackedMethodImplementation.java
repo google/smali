ccbfe537709f0868a84bad07b35d1f5c7864a2f1
Ian Zerny <zerny@google.com>
2023-02-15 11:00:23 +0100

    Don't include debug info in method size.
    The debug info items can be shared across methods so don't count
    them as part of private method size.

--- dexlib2/src/main/java/com/android/tools/smali/dexlib2/dexbacked/DexBackedMethodImplementation.java
+++ dexlib2/src/main/java/com/android/tools/smali/dexlib2/dexbacked/DexBackedMethodImplementation.java
@@ -171,13 +171,11 @@ public class DexBackedMethodImplementation implements MethodImplementation {
     /**
      * Calculate and return the private size of a method implementation.
      *
-     * Calculated as: debug info size + instructions size + try-catch size
+     * Calculated as: instructions size + try-catch size
      *
      * @return size in bytes
      */
     public int getSize() {
-        int debugSize = getDebugInfo().getSize();
-
         //set last offset just before bytecode instructions (after insns_size)
         int lastOffset = getInstructionsStartOffset();
 
@@ -194,7 +192,7 @@ public class DexBackedMethodImplementation implements MethodImplementation {
             lastOffset = ((VariableSizeListIterator)tryHandlerIter).getReaderOffset();
         }
 
-        //method impl size = debug block size + code_item size
-        return debugSize + (lastOffset - codeOffset);
+        //method impl size = code_item size
+        return lastOffset - codeOffset;
     }
 }
